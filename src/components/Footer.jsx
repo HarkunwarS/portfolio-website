f5fc47667104ff0c64c822f76b595885747021bb
HarkunwarS <harkunwarsingh7@gmail.com>
2024-06-15 14:52:16 +0530

    Minor Changes in Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="bg-primary text-white p-5">
       <div className="container mx-auto text-center">
-        &copy; 2023 Harkunwar Singh. All rights reserved.
+        &copy; 2024 Harkunwar Singh. All rights reserved.
       </div>
     </footer>
   );
